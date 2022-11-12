529eb7bd174442a161b3281dfd7e74174c88f9dd
Alexcoder <agboalez@gmail.com>
2022-11-12 17:14:38 +0100

    create button

--- src/components/hero/navHero.js
+++ src/components/hero/navHero.js
@@ -11,20 +11,16 @@ const NavHero = ({onClick1, onClick2, onClick3, onClick4,}) => {
     <div >
       <div style={{width: "17rem", top:"0", margin:"0rem 0rem 0rem 0rem",left:"0", position: "fixed",
                border:"1px solid gray", height:"100vh", background:"linear-gradient(blue, seagreen)",
-                zIndex:"100000", display:"grid", gridTemplateColumns:"repeat(1, 1fr)" }}>
+                zIndex:"100000", display:"flex", flexDirection:"column" }}>
         <div>
           <button 
           onClick={()=> {setOpen(false)}}
           style={{float:"right",width:"2rem"}}><CloseIcon/></button>
         </div>
         <button onClick={onClick1} style={{marginTop:"0.5rem", height:"4rem"}}>CREATE TRANSACTION</button>
-        <hr/>
         <button onClick={onClick2} style={{ height:"4rem"}}>VIEW USER</button>
-        <hr/>
         <button onClick={onClick3} style={{ height:"4rem"}}>OTHER PAGE</button>
-        <hr/>
         <button onClick={onClick4} style={{ height:"4rem"}}>LAST PAGE</button>
-        <hr/>
       </div>
     </div>
   )
