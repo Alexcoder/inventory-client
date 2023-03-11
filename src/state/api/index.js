7b392d40334e8b26ad5eac497f8b2542952d3d6b
Alexcoder <agboalez@gmail.com>
2023-03-11 19:32:37 +0100

    refactor api

--- src/state/api/index.js
+++ src/state/api/index.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 // const token =  JSON.parse(localStorage.getItem("profile")).token;
 
-// const API = axios.create({baseURL: "https://inventory-api-2j2i.onrender.com/api"});
-const API = axios.create({baseURL: "http://127.0.0.1:5000/api" });
+const API = axios.create({baseURL: "https://inventory-api-2j2i.onrender.com/api"});
+// const API = axios.create({baseURL: "http://127.0.0.1:5000/api" });
 // const API = axios.create({baseURL: "http://localhost:5000/api", });
 
 
